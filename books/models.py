9f2e61115eb1bd12e712ee6f53f85d72890643fb
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2025-08-01 10:56:12 +0600

    database model created

--- books/models.py
+++ books/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+# Author Model Later Will Be Replaced By User Model
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+    age = models.IntegerField()
+    country = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    isbn = models.CharField(max_length=13)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
+
+
+# Later Will Be Replaced by User Model
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
--- operations/models.py
+++ operations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from books.models import Book
+from books.models import Member
 
 # Create your models here.
+
+class Borrow(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.DO_NOTHING)
+    member = models.ForeignKey(Member, on_delete=models.DO_NOTHING)
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
